b3f2970b6eb5533f1cea3ab2494770e8544638e4
Segundo Fariña <segundofarina@me.com>
2020-09-18 00:38:19 -0300

    Implment gross margin header

---
+++ Controller/GrossMarginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GrossMarginViewController.swift
+//  LazoReports
+//
+//  Created by Segundo Fariña on 17/09/2020.
+//
+
+import UIKit
+
+class GrossMarginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
